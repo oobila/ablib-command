761bd5f0455fe0398c56529a8a0e88c40ebf4e6c
BuildTools <unconfigured@null.spigotmc.org>
2024-05-24 16:50:16 +0100

    fixing EnumArg

--- src/main/java/com/github/oobila/bukkit/command/arguments/EnumArg.java
+++ src/main/java/com/github/oobila/bukkit/command/arguments/EnumArg.java
@@ -2,20 +2,11 @@ package com.github.oobila.bukkit.command.arguments;
 
 import com.github.oobila.bukkit.command.ArgumentBase;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 
-@SuppressWarnings("unchecked")
 public class EnumArg<T extends Enum<T>> extends ArgumentBase<T, EnumArg<T>> {
     public EnumArg(String name, Class<T> type) {
         super(name, type, arg -> Enum.valueOf(type, arg));
-        try {
-            fixedSuggestions(
-                    Arrays.stream(((T[]) type.getDeclaredMethod("values", String.class).invoke(type, null)))
-                            .toList()
-            );
-        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        }
+        fixedSuggestions(Arrays.stream(type.getEnumConstants()).toList());
     }
 }
